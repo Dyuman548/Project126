7f8128f93785e4eb2e0c4f90bd2af590c71a8751
Dyuman548 <73636626+Dyuman548@users.noreply.github.com>
2021-10-04 19:20:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 song1 = "music.mp3";
 song2 = "music2.mp3";
 
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+
 function preload() 
 {
     song1  = loadSound("music.mp3");
@@ -13,6 +20,29 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Intialized');
+}
+
+function gotPoses(results) 
+{
+    if(results.length > 0)
+    {
+       
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+    }
 }
 
 function draw() {
